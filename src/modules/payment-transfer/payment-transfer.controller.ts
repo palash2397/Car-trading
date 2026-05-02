67879636e125c02fe94c10d1b81ce0c524dbb04c
palash2397 <palashpatil55@gmail.com>
2026-05-02 19:03:25 +0530

    Update payment transfer controller

--- src/modules/payment-transfer/payment-transfer.controller.ts
+++ src/modules/payment-transfer/payment-transfer.controller.ts
@@ -1,7 +1,19 @@
 import { Controller } from '@nestjs/common';
+
+import {
+  ApiBearerAuth,
+  ApiTags,
+  ApiConsumes,
+  ApiBody,
+  ApiParam,
+} from '@nestjs/swagger';
 import { PaymentTransferService } from './payment-transfer.service';
 
+@ApiTags('Payment Transfer')
+@ApiBearerAuth('access-token')
 @Controller('payment-transfer')
 export class PaymentTransferController {
-  constructor(private readonly paymentTransferService: PaymentTransferService) {}
+  constructor(
+    private readonly paymentTransferService: PaymentTransferService,
+  ) {}
 }
