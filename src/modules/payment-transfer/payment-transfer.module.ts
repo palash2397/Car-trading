2257373466a7ee26c324368dfbdce181371e63f4
palash2397 <palashpatil55@gmail.com>
2026-05-06 16:10:58 +0530

    Update payment transfer module with Mongoose imports

--- src/modules/payment-transfer/payment-transfer.module.ts
+++ src/modules/payment-transfer/payment-transfer.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PaymentTransferService } from './payment-transfer.service';
 import { PaymentTransferController } from './payment-transfer.controller';
+
+import { SellerListing, SellerListingSchema } from '../seller-listing/schemas/seller-listing.schema';
+import { PaymentTransfer, PaymentTransferSchema } from './schema/payment-transfer.schema';
 
 @Module({
   controllers: [PaymentTransferController],
   providers: [PaymentTransferService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: SellerListing.name, schema: SellerListingSchema },
+      { name: PaymentTransfer.name, schema: PaymentTransferSchema },
+    ]),
+  ],
+
+  exports: [
+    PaymentTransferService,
+    MongooseModule.forFeature([
+      { name: PaymentTransfer.name, schema: PaymentTransferSchema },
+    ]),
+  ],
 })
 export class PaymentTransferModule {}
