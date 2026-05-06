a7bd966062e4100106fca9dfa663e13d3d8ccd5e
palash2397 <palashpatil55@gmail.com>
2026-05-06 16:16:21 +0530

    Add constructor and dependencies to payment transfer service

--- src/modules/payment-transfer/payment-transfer.service.ts
+++ src/modules/payment-transfer/payment-transfer.service.ts
@@ -10,5 +10,16 @@ import {
   SellerListingDocument,
 } from '../seller-listing/schemas/seller-listing.schema';
 
+
+import { ApiResponse } from 'src/utils/helpers/ApiResponse';
+import { Msg } from 'src/utils/helpers/responseMsg';
+
 @Injectable()
-export class PaymentTransferService {}
+export class PaymentTransferService {
+  constructor(
+    @InjectModel(PaymentTransfer.name)
+    private readonly paymentTransferModel: Model<PaymentTransferDocument>,
+    @InjectModel(SellerListing.name)
+    private readonly sellerListingModel: Model<SellerListingDocument>,
+  ) {}
+}
