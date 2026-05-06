9d9af748b74c34beae9b282e31a28071c898deda
palash2397 <palashpatil55@gmail.com>
2026-05-06 16:12:47 +0530

    Add imports to payment transfer service

--- src/modules/payment-transfer/payment-transfer.service.ts
+++ src/modules/payment-transfer/payment-transfer.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import {
+  PaymentTransfer,
+  PaymentTransferDocument,
+} from './schema/payment-transfer.schema';
+import {
+  SellerListing,
+  SellerListingDocument,
+} from '../seller-listing/schemas/seller-listing.schema';
 
 @Injectable()
 export class PaymentTransferService {}
